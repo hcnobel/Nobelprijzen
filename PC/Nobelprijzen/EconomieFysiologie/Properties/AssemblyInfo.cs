d7a6a997c81a53e52dbf5c01b11a950a6c96687a
Erwin de Haan <git@erayan.eu>
2013-05-29 17:54:06 +0200

    It works.

--- PC/Nobelprijzen/EconomieFysiologie/Properties/AssemblyInfo.cs
+++ PC/Nobelprijzen/EconomieFysiologie/Properties/AssemblyInfo.cs
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("0.5.29.7")]
 [assembly: AssemblyFileVersion("2014.2.19.33")]
